d571f674c42bbb4391edb45918c4e6dc11c0b2ff
Muhammad Owais <muhammadowaisofficial009@gmail.com>
2023-03-16 10:49:54 -0700

    updated

--- client/src/components/Home/Category/Category.jsx
+++ client/src/components/Home/Category/Category.jsx
@@ -1,21 +1,62 @@
 
 import "./Category.scss";
-// import cat1 from "../../../assets/category/cat-1.jpg";
+import cat1 from "../../../assets/category/cat-1.jpg";
 
-const Category = ({categories}) => {
+const Category = () => {
+// const Category = ({categories}) => {
 
     return ( 
+    // <div className="shop-by-category">
+    //     <div className="categories">
+    //         {categories.data.map((item) => (
+
+    //             <div key={item.id} className="category">
+    //             <img src={process.env.REACT_APP_DEV_URL + item.attributes.img.data.attributes.url} alt="" />
+    //             </div>
+
+    //         ))}
+    //     </div>
+    // </div>
+
     <div className="shop-by-category">
         <div className="categories">
-            {categories.data.map((item) => (
 
-                <div key={item.id} className="category">
-                <img src={process.env.REACT_APP_DEV_URL + item.attributes.img.data.attributes.url} alt="" />
+                <div className="category">
+                <img src={cat1} alt="" />
                 </div>
 
-            ))}
+                <div className="category">
+                <img src={cat1} alt="" />
+                </div>
+
+                <div className="category">
+                <img src={cat1} alt="" />
+                </div>
+
+                <div className="category">
+                <img src={cat1} alt="" />
+                </div>
+
+                <div className="category">
+                <img src={cat1} alt="" />
+                </div>
+
+                <div className="category">
+                <img src={cat1} alt="" />
+                </div>
+
+                <div className="category">
+                <img src={cat1} alt="" />
+                </div>
+
+                <div className="category">
+                <img src={cat1} alt="" />
+                </div>
+
         </div>
     </div>
+
+
     );
 };
 
